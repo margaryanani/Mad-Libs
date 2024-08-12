d455f0e293bca3dfb035448529a77a3ae6c16fa3
Ani Margaryan <ani.m.yan55@gmail.com>
2024-08-12 21:26:24 +0400

    Limited the lines to 79 chars

--- madlibs.py
+++ madlibs.py
@@ -1,4 +1,3 @@
-import random
 from madlibs_functions import get_prompts, inputs
 
 input("""
@@ -13,48 +12,71 @@ Press Enter to start the game.
 """)
 print("Starting...")
 
-# categories with their corresponding words to generate a word if needed.
+# Categories with their corresponding words to generate a word if needed.
 categories = {
     "number": [3, 14, 27, 8, 21, 35, 50, 12, 19, 28, 33, 45, 60, 7, 25],
-    "measure of time": ["second", "minute", "hour", "day", "week", "month", "year", "decade", "century", "millennium",
-                        "microsecond", "millisecond", "nanosecond", "fortnight", "bimester"],
-    "mode of transportation": ["car", "bicycle", "bus", "train", "airplane", "boat", "motorcycle", "scooter", "subway",
-                               "tram", "helicopter", "skateboard", "rollerblades", "hot air balloon", "ferry"],
-    "adjective": ["happy", "sad", "bright", "dark", "quick", "slow", "loud", "quiet", "large", "small", "hot", "cold",
+    "measure of time": ["second", "minute", "hour", "day", "week", "month",
+                        "year", "decade", "century", "millennium",
+                        "microsecond", "millisecond", "nanosecond",
+                        "fortnight", "bimester"],
+    "mode of transportation": ["car", "bicycle", "bus", "train", "airplane",
+                               "boat", "motorcycle", "scooter", "subway",
+                               "tram", "helicopter", "skateboard",
+                               "rollerblades", "hot air balloon", "ferry"],
+    "adjective": ["happy", "sad", "bright", "dark", "quick", "slow", "loud",
+                  "quiet", "large", "small", "hot", "cold",
                   "young", "old", "brave"],
-    "noun": ["dog", "cat", "tree", "car", "house", "book", "computer", "phone", "mountain", "river", "city", "ocean",
-             "flower", "bicycle", "bird"],
-    "color": ["red", "blue", "green", "yellow", "purple", "orange", "pink", "brown", "black", "white", "gray", "cyan",
+    "noun": ["dog", "cat", "tree", "car", "house", "book", "computer", "phone",
+             "mountain", "river", "city", "ocean", "flower",
+             "bicycle", "bird"],
+    "color": ["red", "blue", "green", "yellow", "purple", "orange", "pink",
+              "brown", "black", "white", "gray", "cyan",
               "magenta", "lime", "teal"],
-    "part of the body": ["head", "arm", "leg", "hand", "foot", "eye", "ear", "nose", "mouth", "back", "shoulder",
+    "part of the body": ["head", "arm", "leg", "hand", "foot", "eye", "ear",
+                         "nose", "mouth", "back", "shoulder",
                          "knee", "elbow", "wrist", "ankle"],
-    "verb": ["run", "jump", "swim", "dance", "eat", "sing", "write", "read", "fly", "laugh", "play", "paint", "cook",
-             "sleep", "talk"],
-    "silly word": ["blubber", "flibbertigibbet", "gobbledygook", "snollygoster", "lollygag", "bamboozle", "kerfuffle",
-                   "hootenanny", "skedaddle", "fiddle-faddle", "jabberwocky", "dinglehopper", "widdershins",
-                   "gobbledygook", "nincompoop"],
-    "proper noun": ["Alice", "Bob", "Charlie", "Diana", "Ethan", "Fiona", "George", "Hannah", "Ian", "Julia", "Kevin",
-                    "Laura", "Michael", "Nina", "Oliver"],
-    "adjective (feeling)": ["happy", "sad", "angry", "excited", "nervous", "calm", "bored", "confused", "anxious",
-                            "joyful", "frustrated", "relaxed", "hopeful", "scared", "content"],
-    "animal": ["dog", "cat", "elephant", "lion", "tiger", "giraffe", "monkey", "bear", "rabbit", "snake", "deer", "fox",
-               "penguin", "dolphin", "turtle"],
-    "verb (ending in ing)": ["singing", "dancing", "eating", "drinking", "swinging", "painting", "running", "walking",
-                             "sleeping", "thinking", "playing", "writing", "reading", "laughing", "crying"],
-    "adverb (ending in ly)": ["quickly", "slowly", "happily", "sadly", "loudly", "quietly", "calmly", "nervously",
-                              "excitedly", "carefully", "eagerly", "kindly", "boldly", "gently", "firmly"],
-    "place": ["beach", "park", "mountain", "city", "forest", "desert", "lake", "river", "island", "castle",
-              "countryside", "library", "cafe", "school", "hospital"],
-    "magical creature (plural)": ["dragons", "unicorns", "fairies", "elves", "phoenixes", "centaurs", "mermaids",
-                                  "werewolves", "griffins", "goblins", "sprites", "vampires", "sphinxes", "pegasi",
+    "verb": ["run", "jump", "swim", "dance", "eat", "sing", "write", "read",
+             "fly", "laugh", "play", "paint", "cook", "sleep", "talk"],
+    "silly word": ["blubber", "flibbertigibbet", "gobbledygook",
+                   "snollygoster", "lollygag", "bamboozle",
+                   "kerfuffle", "hootenanny", "skedaddle",
+                   "fiddle-faddle", "jabberwocky", "dinglehopper",
+                   "widdershins", "gobbledygook", "nincompoop"],
+    "proper noun": ["Alice", "Bob", "Charlie", "Diana", "Ethan",
+                    "Fiona", "George", "Hannah", "Ian", "Julia",
+                    "Kevin", "Laura", "Michael", "Nina", "Oliver"],
+    "adjective (feeling)": ["happy", "sad", "angry", "excited", "nervous",
+                            "calm", "bored", "confused", "anxious", "joyful",
+                            "frustrated", "relaxed", "hopeful",
+                            "scared", "content"],
+    "animal": ["dog", "cat", "elephant", "lion", "tiger",
+               "giraffe", "monkey", "bear", "rabbit", "snake",
+               "deer", "fox", "penguin", "dolphin", "turtle"],
+    "verb (ending in ing)": ["singing", "dancing", "eating", "drinking",
+                             "swinging", "painting", "running", "walking",
+                             "sleeping", "thinking", "playing", "writing",
+                             "reading", "laughing", "crying"],
+    "adverb (ending in ly)": ["quickly", "slowly", "happily", "sadly",
+                              "loudly", "quietly", "calmly", "nervously",
+                              "excitedly", "carefully", "eagerly", "kindly",
+                              "boldly", "gently", "firmly"],
+    "place": ["beach", "park", "mountain", "city", "forest", "desert", "lake",
+              "river", "island", "castle", "countryside", "library", "cafe",
+              "school", "hospital"],
+    "magical creature (plural)": ["dragons", "unicorns", "fairies", "elves",
+                                  "phoenixes", "centaurs", "mermaids",
+                                  "werewolves", "griffins", "goblins",
+                                  "sprites", "vampires", "sphinxes", "pegasi",
                                   "djinn"],
-    "room in a house": ["kitchen", "living room", "bedroom", "bathroom", "dining room", "study", "guest room",
-                        "playroom", "basement", "attic", "laundry room", "garage", "sunroom", "mudroom"],
-    "noun (plural)": ["students", "teachers", "chairs", "tables", "pens", "keys", "shoes", "socks", "glasses",
-                      "watches", "clouds", "stars", "fish", "horses", "butterflies"]
+    "room in a house": ["kitchen", "living room", "bedroom", "bathroom",
+                        "dining room", "study", "guest room",
+                        "playroom", "basement", "attic", "laundry room",
+                        "garage", "sunroom", "mudroom"],
+    "noun (plural)": ["students", "teachers", "chairs", "tables", "pens",
+                      "keys", "shoes", "socks", "glasses", "watches", "clouds",
+                      "stars", "fish", "horses", "butterflies"]
 }
-
-# prompts with their categories for templates 1,2 and 3 combined.
+# Prompts with their categories for templates 1,2 and 3 combined.
 prompts_and_categories = [
     ("Enter number --> ", "number"),  # 0
     ("Enter measure of time --> ", "measure of time"),  # 1
@@ -72,11 +94,12 @@ prompts_and_categories = [
     ("Enter Verb (ending in ing) --> ", "verb (ending in ing)"),  # 13
     ("Enter Adverb (ending in ly) --> ", "adverb (ending in ly)"),  # 14
     ("Enter Place --> ", "place"),  # 15
-    ("Enter Magical Creature (Plural) --> ", "magical creature (plural)"),  # 16
+    ("Enter Magical Creature (Plural) --> ", "magical creature (plural)"), # 16
     ("Enter Room in a House --> ", "room in a house"),  # 17
 ]
 
-# separated prompts for each template.
+# Separated prompts (based on the indices of prompts_and_categories)
+# for each template.
 template_mapping = {
     '1': [0, 1, 2, 3, 3, 4, 5, 6, 7, 0, 8, 8, 6, 7, 8, 3, 9, 8],
     '2': [10, 8, 11, 7, 11, 12, 7, 5, 13, 14, 0, 1, 5, 12, 0, 9, 8],
@@ -90,23 +113,27 @@ while template not in ['1', '2', '3']:
     template = input('Choose a template (1, 2, or 3): ')
 
 
-# function to get the prompts needed for a specified template
+# Function to get the prompts needed for a specified template number.
 prompts = get_prompts(template_mapping, prompts_and_categories, template)
 
-# function to give template's prompts to the user and store them in a list
+# Function to give template's prompts to the user and store the entered words
+# in a list.
 user_inputs = inputs(prompts, categories)
 
 # template 1
 if template == '1':
     print(f"""
-It was about [{user_inputs[0]}] [{user_inputs[1]}] ago when I arrived at the hospital in a [{user_inputs[2]}]. 
-The hospital is a/an [{user_inputs[3]}] place, there are a lot of [{user_inputs[4]}] [{user_inputs[5]}] here. 
-There are nurses here who have [{user_inputs[6]}] [{user_inputs[7]}]. If someone wants to come into 
-my room I told them that they have to [{user_inputs[8]}] first. I’ve decorated my room with 
-[{user_inputs[9]}] [{user_inputs[10]}].Today I talked to a doctor and they were wearing a [{user_inputs[11]}]
-on their [{user_inputs[12]}]. I heard that all doctors [{user_inputs[13]}] [{user_inputs[14]}] everyday for
-breakfast. The most [{user_inputs[15]}] thing about being in the hospital is the [{user_inputs[16]}]
-[{user_inputs[17]}]!
+It was about [{user_inputs[0]}] [{user_inputs[1]}] ago when I arrived at the 
+hospital in a [{user_inputs[2]}]. The hospital is a/an [{user_inputs[3]}]
+place, there are a lot of [{user_inputs[4]}] [{user_inputs[5]}] here. 
+There are nurses here who have [{user_inputs[6]}] [{user_inputs[7]}].
+If someone wants to come into my room I told them that they have to
+[{user_inputs[8]}] first. I’ve decorated my room with 
+[{user_inputs[9]}] [{user_inputs[10]}].Today I talked to a doctor and they were
+wearing a [{user_inputs[11]}] on their [{user_inputs[12]}]. I heard that all
+doctors [{user_inputs[13]}] [{user_inputs[14]}] everyday for breakfast. The
+most [{user_inputs[15]}] thing about being in the hospital is
+the [{user_inputs[16]}] [{user_inputs[17]}]!
 """)
 
 
@@ -114,22 +141,29 @@ breakfast. The most [{user_inputs[15]}] thing about being in the hospital is the
 elif template == '2':
     print(f"""
 This weekend I am going camping with [{user_inputs[0]}]. I packed my lantern, 
-sleeping bag, and [{user_inputs[1]}]. I am so [{user_inputs[2]}] to [{user_inputs[3]}] in a tent. I am [{user_inputs[4]}]
-we might see a(n) [{user_inputs[5]}], I hear they're kind of dangerous. While we’re camping, we are going to hike,
-fish, and [{user_inputs[6]}]. I have heard that the [{user_inputs[7]}] lake is great for [{user_inputs[8]}]. 
-Then we will [{user_inputs[9]}] hike through the forest for [{user_inputs[10]}] [{user_inputs[11]}]. If I see
-a [{user_inputs[12]}] [{user_inputs[13]}] while hiking, I am going to bring it home as a pet! At night we will tell 
-[{user_inputs[14]}] [{user_inputs[15]}] stories and roast [{user_inputs[16]}] around the campfire!!
+sleeping bag, and [{user_inputs[1]}]. I am so [{user_inputs[2]}] to
+[{user_inputs[3]}] in a tent. I am [{user_inputs[4]}] we might see
+a(n) [{user_inputs[5]}], I hear they're kind of dangerous. While we’re camping,
+we are going to hike, fish, and [{user_inputs[6]}]. I have heard that the
+[{user_inputs[7]}] lake is great for [{user_inputs[8]}]. Then we will 
+[{user_inputs[9]}] hike through the forest for [{user_inputs[10]}] 
+[{user_inputs[11]}]. If I see a [{user_inputs[12]}] [{user_inputs[13]}] while 
+hiking, I am going to bring it home as a pet! At night we will tell 
+[{user_inputs[14]}] [{user_inputs[15]}] stories and roast [{user_inputs[16]}] 
+around the campfire!!
 """)
 
 # template 3
 elif template == '3':
     print(f"""
-Dear [{user_inputs[0]}], I am writing to you from a [{user_inputs[1]}] castle in
-an enchanted forest. I found myself here one day after going for a ride on a [{user_inputs[2]}] [{user_inputs[3]}]
-in [{user_inputs[4]}]. There are [{user_inputs[5]}] [{user_inputs[6]}] and [{user_inputs[7]}] [{user_inputs[8]}] here! 
-In the [{user_inputs[9]}] there is a pool full of [{user_inputs[10]}]. I fall asleep each night
-on a [{user_inputs[11]}] of [{user_inputs[12]}] and dream of [{user_inputs[13]}] [{user_inputs[14]}]. It feels as though
-I have lived here for [{user_inputs[15]}] [{user_inputs[16]}]. I hope one day you can visit, although the only
-way to get here now is [{user_inputs[17]}] on a [{user_inputs[18]}] [{user_inputs[19]}]!!
+Dear [{user_inputs[0]}], I am writing to you from a [{user_inputs[1]}] castle
+in an enchanted forest. I found myself here one day after going for a ride on a
+[{user_inputs[2]}] [{user_inputs[3]}] in [{user_inputs[4]}]. There are 
+[{user_inputs[5]}] [{user_inputs[6]}] and [{user_inputs[7]}] [{user_inputs[8]}]
+here! In the [{user_inputs[9]}] there is a pool full of [{user_inputs[10]}].
+I fall asleep each night on a [{user_inputs[11]}] of [{user_inputs[12]}] and 
+dream of [{user_inputs[13]}] [{user_inputs[14]}]. It feels as though I have 
+lived here for [{user_inputs[15]}] [{user_inputs[16]}]. I hope one day you can
+visit, although the only way to get here now is [{user_inputs[17]}] on a
+[{user_inputs[18]}] [{user_inputs[19]}]!!
 """)
